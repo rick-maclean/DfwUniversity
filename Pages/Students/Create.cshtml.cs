9f633dfae46c87c1ed9abeb29e7a9a22589d9e13
Richard MacLean <RMacLean@swbts.edu>
2021-03-16 14:00:38 -0500

    change code to prevent overposting attacks

--- Pages/Students/Create.cshtml.cs
+++ Pages/Students/Create.cshtml.cs
@@ -28,17 +28,34 @@ namespace DfwUniversity.Pages_Students
         public Student Student { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        // public async Task<IActionResult> OnPostAsync()
+        // {
+        //     if (!ModelState.IsValid)
+        //     {
+        //         return Page();
+        //     }
+
+        //     _context.Students.Add(Student);
+        //     await _context.SaveChangesAsync();
+
+        //     return RedirectToPage("./Index");
+        // }
+
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var emptyStudent = new Student();
+
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate, s => s.EmailAddress))
             {
-                return Page();
+                _context.Students.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
 
-            _context.Students.Add(Student);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            return Page();
         }
     }
 }
--- Pages/Students/Edit.cshtml.cs
+++ Pages/Students/Edit.cshtml.cs
@@ -30,7 +30,9 @@ namespace DfwUniversity.Pages_Students
                 return NotFound();
             }
 
-            Student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+            // Student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id); 
+            Student = await _context.Students.FindAsync(id); //tmore efficient when FirstOrDefaultAsync is not needed for related data
+
 
             if (Student == null)
             {
@@ -41,32 +43,52 @@ namespace DfwUniversity.Pages_Students
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        // public async Task<IActionResult> OnPostAsync()
+        // {
+        //     if (!ModelState.IsValid)
+        //     {
+        //         return Page();
+        //     }
+
+        //     _context.Attach(Student).State = EntityState.Modified;
+
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!StudentExists(Student.ID))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
+
+        //     return RedirectToPage("./Index");
+        // }
+        public async Task<IActionResult> OnPostAsync(int id)
         {
-            if (!ModelState.IsValid)
+            var studentToUpdate = await _context.Students.FindAsync(id);
+
+            if (studentToUpdate == null)
             {
-                return Page();
+                return NotFound();
             }
 
-            _context.Attach(Student).State = EntityState.Modified;
-
-            try
+            if (await TryUpdateModelAsync<Student>(
+                studentToUpdate,
+                "student",
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate, s => s.EmailAddress))
             {
                 await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(Student.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return RedirectToPage("./Index");
             }
 
-            return RedirectToPage("./Index");
+            return Page();
         }
 
         private bool StudentExists(int id)
