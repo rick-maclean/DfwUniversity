d06869713770c8fb8ecf87d30249d7726388f0f1
Richard MacLean <RMacLean@swbts.edu>
2021-03-17 13:52:41 -0500

    added search on first/last name

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -32,32 +32,40 @@ namespace DfwUniversity.Pages_Students
         // {
         //     Students = await _context.Students.ToListAsync();
         // }
-        public async Task OnGetAsync(string sortOrder)
-    {
-        // using System;
-        NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-        DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-
-        IQueryable<Student> studentsIQ = from s in _context.Students
-                                        select s;
-
-        switch (sortOrder)
+        //public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            case "name_desc":
-                studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
-                break;
-            case "Date":
-                studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
-                break;
-            case "date_desc":
-                studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
-                break;
-            default:
-                studentsIQ = studentsIQ.OrderBy(s => s.LastName);
-                break;
-        }
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
-        Students = await studentsIQ.AsNoTracking().ToListAsync();
-    }
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                            select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                || s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
+        }
     }
 }
