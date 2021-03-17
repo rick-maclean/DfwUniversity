a04d8c4e34b12b6a8b7fe233d8968a087f44b53e
Richard MacLean <RMacLean@swbts.edu>
2021-03-17 15:54:15 -0500

    Added code for About page which shows how many students are enrolled per enrollmentDate using the SQL groupBy operation

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,37 @@
+using DfwUniversity.Models.SchoolViewModels;
+using DfwUniversity.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DfwUniversity.Models;
+
+namespace DfwUniversity.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly SchoolContext _context;
+        
+        public AboutModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> StudentsByDate {get; set;}
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup 
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            StudentsByDate = await data.AsNoTracking().ToListAsync();
+        }
+    }
+}
