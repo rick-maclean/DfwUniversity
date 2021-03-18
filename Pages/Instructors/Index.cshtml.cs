522239565dd0f717c8267899b2a9326e21171828
Richard MacLean <rmaclean@swbts.edu>
2021-03-18 15:04:02 -0500

    generated code so that Instructors and Departments have CRUD and view in the web app

---
+++ Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DfwUniversity.Data;
+using DfwUniversity.Models;
+
+namespace DfwUniversity.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DfwUniversity.Data.SchoolContext _context;
+
+        public IndexModel(DfwUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Instructor> Instructor { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Instructor = await _context.Instructors.ToListAsync();
+        }
+    }
+}
