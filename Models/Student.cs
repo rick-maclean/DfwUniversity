2184779588af5addff7f72532efaad272fe0beca
Richard MacLean <rmaclean@swbts.edu>
2021-03-18 12:03:53 -0500

    modified Student to have FirstName vs FisrtMidName and created the migration and ran it which updates the DB

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,15 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DfwUniversity.Models
 {
     public class Student
     {
         public int ID {get; set;}
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Display(Name = "Last Name")]
         public string LastName {get; set;}
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [StringLength(50, ErrorMessage = "First name cannot BE longer than 50 characters.", MinimumLength = 1)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName {get; set;}
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address")]
         public string EmailAddress {get; set;}
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate {get; set;}
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + "' " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments {get; set;}
 
