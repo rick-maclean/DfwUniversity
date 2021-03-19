cb49963ad5eeddf0e6dbca520c35639d9307720e
Richard MacLean <rmaclean@swbts.edu>
2021-03-19 14:25:36 -0500

    Add the instructor pages and the ability to show thier classes and students for those classes

--- Pages/Instructors/Index.cshtml.cs
+++ Pages/Instructors/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DfwUniversity.Data;
 using DfwUniversity.Models;
+using DfwUniversity.Models.SchoolViewModels;
 
 namespace DfwUniversity.Pages.Instructors
 {
@@ -19,11 +20,47 @@ namespace DfwUniversity.Pages.Instructors
             _context = context;
         }
 
-        public IList<Instructor> Instructor { get;set; }
+        public int InstructorID {get; set;}
+        public int CourseID {get;set;}
+        public InstructorIndexData InstructorData {get; set;}
 
-        public async Task OnGetAsync()
+        //public IList<Instructor> Instructor { get;set; }
+
+        // public async Task OnGetAsync()
+        // {
+        //     Instructor = await _context.Instructors.ToListAsync();
+        // }
+        public async Task OnGetAsync(int? id, int? courseID)
         {
-            Instructor = await _context.Instructors.ToListAsync();
+            InstructorData = new InstructorIndexData();
+            InstructorData.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)                 
+                .Include(i => i.CourseAssignments)
+                    .ThenInclude(i => i.Course)
+                        .ThenInclude(i => i.Department)
+                .Include(i => i.CourseAssignments)
+                    .ThenInclude(i => i.Course)
+                        .ThenInclude(i => i.Enrollments)
+                            .ThenInclude(i => i.Student)
+                .AsNoTracking()
+                .OrderBy(i => i.LastName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                InstructorID = id.Value;
+                Instructor instructor = InstructorData.Instructors
+                    .Where(i => i.ID == id.Value).Single();
+                InstructorData.Courses = instructor.CourseAssignments.Select(s => s.Course);
+            }
+
+            if (courseID != null)
+            {
+                CourseID = courseID.Value;
+                var selectedCourse = InstructorData.Courses
+                    .Where(x => x.CourseID == courseID).Single();
+                InstructorData.Enrollments = selectedCourse.Enrollments;
+            }
         }
     }
 }
