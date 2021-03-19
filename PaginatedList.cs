a1bf59ade9f2ba5cb7d9e0fccc18def04cd9cf5d
Richard MacLean <rmaclean@swbts.edu>
2021-03-19 13:31:23 -0500

    commit some minor edits whcih were not saved before on these files

--- PaginatedList.cs
+++ PaginatedList.cs
@@ -38,7 +38,7 @@ namespace DfwUniversity
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
             var count = await source.CountAsync();
-            var items = await source.Skip( (pageIndex-1) * pageSize ).Take(pageSize).ToListAsync();
+            var items = await source.Skip( (pageIndex - 1) * pageSize ).Take(pageSize).ToListAsync();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
