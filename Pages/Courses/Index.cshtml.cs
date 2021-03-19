3516dfcff3eb6d38eee9e2df76b2fce1643b5e2f
Richard MacLean <rmaclean@swbts.edu>
2021-03-19 10:13:52 -0500

    there are the files the code generator produced

--- Pages/Courses/Index.cshtml.cs
+++ Pages/Courses/Index.cshtml.cs
@@ -19,16 +19,12 @@ namespace DfwUniversity.Pages.Courses
             _context = context;
         }
 
-        public IList<Course> Courses { get;set; }
+        public IList<Course> Course { get;set; }
 
         public async Task OnGetAsync()
         {
-            // Courses = await _context.Courses
-            //     .Include(c => c.Department)
-            //     .AsNoTracking().ToListAsync();
-            Courses = await _context.Courses
-                .Include(c => c.Department)
-                .ToListAsync();
+            Course = await _context.Courses
+                .Include(c => c.Department).ToListAsync();
         }
     }
 }
